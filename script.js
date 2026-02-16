ce55c7c21cbce200d95c94b54a17a756642ca34b
fmahadihasan1 <mahadihasan18407@gmail.com>
2026-02-16 22:38:53 +0400

    loading categories from API

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log("consoled from script js file");
+const getCategoriesURL = "https://fakestoreapi.com/products/categories";
+
+// fetch categories from api and display them in the page
+const getCategories = async () => {
+  const response = await fetch(getCategoriesURL);
+
+  const categories = await response.json();
+
+  displayCategories(categories);
+  displayCategoriesResponsive(categories);
+};
+
+// show categories in normal
+const displayCategories = (categories) => {
+  const liContainer = document.getElementById("categories-container");
+
+  categories.forEach((category) => {
+    const categoryElement = document.createElement("li");
+    categoryElement.classList.add(
+      "hover:bg-blue-100",
+
+      "rounded-lg",
+      "px-2",
+      "font-bold",
+
+      "transition",
+      "duration-500",
+      "ease-out",
+      "hover:bg-indigo-500",
+      "hover:text-white",
+    );
+
+    categoryElement.innerHTML = `<a class="capitalize">${category}</a>`;
+    liContainer.appendChild(categoryElement);
+  });
+};
+
+// show categories in responsive
+const displayCategoriesResponsive = (categories) => {
+  const liContainerResponsive = document.getElementById(
+    "categories-container-respnsive",
+  );
+  categories.forEach((category) => {
+    const categoryElement = document.createElement("li");
+
+    categoryElement.innerHTML = `<a class="capitalize">${category}</a>`;
+    liContainerResponsive.appendChild(categoryElement);
+  });
+};
+
+getCategories();
